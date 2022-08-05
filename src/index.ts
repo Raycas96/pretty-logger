1548f74016a52ee132aeb32b14fc0dac83fcd7cc
Giuseppe Petriccione <39119454+Raycas96@users.noreply.github.com>
2022-08-05 09:36:56 +0200

    removed usless code

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,4 @@
 import PrettyLogger from './logger/logger';
-import { logger } from './logger/utils';
 
 export { logger } from './logger/utils';
 export const Logger = PrettyLogger;
-
-
-logger.info('Hello', { world: 'test' }, 'lorem');
-logger.error('Hello', { world: 'test' }, 'lorem');
-logger.warn('Hello', { world: 'test' }, 'lorem'); //[⚠WARN]: [Hello]
